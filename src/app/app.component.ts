8ac04b2bb3081b556035eeba20c266178e2b5cbd
ibsan <ibsan_89@hotmail.com>
2020-10-07 22:44:44 -0700

    Add event component and list event and odd number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-sample';
+  oddNumbers:number[] = [];
+  eventNumbers:number[] = [];
 
   onIntervalFired(firedNumber : number){
     console.log(firedNumber);
+    if(firedNumber % 2 == 0){
+      this.eventNumbers.push(firedNumber);
+    }else{
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
